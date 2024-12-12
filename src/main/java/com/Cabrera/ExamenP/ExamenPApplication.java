01bdc86c5436e0f76d6e49cf1e680325bc4e7541
Narumi420 <cpiscoyanarumis@uss.edu.pe>
2024-12-12 00:32:11 -0500

    Add files via upload

---
+++ src/main/java/com/Cabrera/ExamenP/ExamenPApplication.java
@@ -0,0 +1,13 @@
+package com.Cabrera.ExamenP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenPApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Cabrera/ExamenP/ExamenPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Cabrera.ExamenP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
